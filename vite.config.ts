01144ddd032c60552e42e3494291ccd6a179066b
JRA646 <149916056+JRA646@users.noreply.github.com>
2026-01-29 05:31:36 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/flc-application/",
+  base: "/flc-application/",
   envPrefix: 'VITE_',
   resolve: {
     alias: {
